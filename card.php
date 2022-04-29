c3b7b7ad59f636b10c263d92dea4ec046550f497
maxhltz <83210032+maxhltz@users.noreply.github.com>
2022-04-29 10:37:44 +0200

    main.html

---
+++ card.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <img src="https://placekitten.com/286/180" class="card-img-top" alt="Produktbild">
+    <div class="card-body">
+        Hier könnte die Produktbeschreibung stehen!
+    </div>
+    <div class="card-footer">
+        <a href="" class="btn btn-primary btn-sm">Details</a>
+        <a href="" class="btn btn-success btn-sm">In den Warenkorb</a>
+
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors','On');
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Candyland</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Assets/css/styles.css">
+</head>
+
+<body>
+    <header class="jumbotron">
+        <div class="container">
+            <h1>Willkommen im Candylan</h1>
+        </div>
+    </header>
+    <section class="container" id="products">
+        <div class="row">
+            <div class="col">
+                <?php include 'card.php'?>
+            </div>
+            <div class="col">
+                <?php include 'card.php'?>
+            </div>
+            <div class="col">
+                <?php include 'card.php'?>
+            </div>
+            <div class="col">
+                <?php include 'card.php'?>
+            </div>
+        </div>
+    </section>
+    <script src="Assets/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
